6f871b11a3954732034a13c884e060986ec5fd89
akouminov <Main1user!>
2020-02-24 13:53:01 -0500

    small changes

--- youtube_scrape.py
+++ youtube_scrape.py
@@ -11,3 +11,10 @@ from selenium.webdriver.support import expected_conditions as EC
 #https://www.youtube.com/user/TEDtalksDirector
 
 #https://www.youtube.com/user/bbcnews
+
+#need a way to quantify if a video is good or not
+#ask about the software that lines up text
+#would be nice to run this on a server that is continuously running this
+
+driver = webdriver.Chrome()
+driver.get("YOUR_LINK_HERE")
