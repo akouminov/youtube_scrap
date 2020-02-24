3cbae43e338c074b1503c13eff1f1860fb5e33dc
akouminov <Main1user!>
2020-02-24 13:45:19 -0500

    Initial commit

---
+++ youtube_scrape.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+import pandas as pd
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# https://www.youtube.com/
+
+#https://www.youtube.com/results?search_query=bbc
+
+#https://www.youtube.com/user/TEDtalksDirector
+
+#https://www.youtube.com/user/bbcnews
